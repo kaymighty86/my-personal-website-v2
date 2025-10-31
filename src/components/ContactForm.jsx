c1e05a9724569c9847940905c453fcb1490a6c7c
Olukayode Adeoye Simeon <kaymighty86@gmail.com>
2025-10-31 17:17:08 +0000

    Implemented some UI updates
    - Updated the Navigation menu to dynamically adapt to the screen of mobile browsers.
    - Updated the Contact process UI flow by showing a loading animation.
    - Cap the width of the data layer of the landing section for wide screens

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -18,6 +18,9 @@ export default function ContactForm(){
     async function handleFormSubmit(prevFormData, formData){
         //BEGINNING SUBMISSION
         formData.append("access_key", "f695bdde-9541-4566-9fe9-d66d709cf208");
+
+        formData.set("name",formData.get("name").trim());//trim any leading or trailing whitespaces in the name
+        formData.set("message",formData.get("message").trim());//trim any leading or trailing whitespaces in the messgae
         
         try{
             const response = await fetch("https://api.web3forms.com/submit",{
@@ -63,12 +66,18 @@ export default function ContactForm(){
             </p>}
             {isSubmitting && <p className={`${styles.formResponseMessage}`}>Submitting your message...</p>}
             <form className={styles.contactForm} action={dispatchFormAction}>
-                <Input label={"Name"} type="text" name="name" defaultValue={formState && formState.name}  required placeholder="Your name (include your title if necessary)" disabled={isSubmitting}/>
-                <Input label={"Email"} type="email" name="email" defaultValue={formState && formState.email} required placeholder="Your email address" disabled={isSubmitting}/>
-                <TextArea label={"message"} name="message" defaultValue={formState && formState.message} required placeholder="Type your message 😃" disabled={isSubmitting}></TextArea>
+                {!isSubmitting? <span>
+                        <Input label={"Name"} type="text" name="name" defaultValue={formState && formState.name}  required placeholder="Your name (include your title if necessary)" disabled={isSubmitting}/>
+                        <Input label={"Email"} type="email" name="email" defaultValue={formState && formState.email} required placeholder="Your email address" disabled={isSubmitting}/>
+                        <TextArea label={"message"} name="message" defaultValue={formState && formState.message} required placeholder="Type your message 😃" disabled={isSubmitting}></TextArea>
+                    </span>:
+                    <span className={styles.formLoadingUISection}>
+                        <MdOutgoingMail className={styles.loadingIcon}/>
+                    </span>
+                }                 
                 <Button className={styles.submitFormBtn} disabled={isSubmitting}>
                     {!isSubmitting? <>Send <FaPaperPlane /></> : 
-                    <>Sending... <MdOutgoingMail className={styles.bobbingAnim}/></>}
+                    <>Sending...</>}
                 </Button>
             </form>
         </>
